4d0cf6dfd76487e5685879c80e396780512033c1
Phil Paolo <philipcpaolo@gmail.com>
2020-11-18 08:42:54 -0500

    Add a timer to the game

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -7,15 +7,38 @@ using System.Windows.Input;
 
 namespace MatchGame
 {
+    using System.Windows.Threading;
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Timer
+        DispatcherTimer timer = new DispatcherTimer();
+        // Time that has elapsed
+        int tenthsOfSecondElapsed;
+        // Number of matches the player found
+        int matchesFound;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
             SetUpGame();
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tenthsOfSecondElapsed++;
+            timeTextBlock.Text = (tenthsOfSecondElapsed / 10F).ToString("0.0s");
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                timeTextBlock.Text += " - Play again?";
+            }
         }
 
         private void SetUpGame()
@@ -40,15 +63,23 @@ namespace MatchGame
             // Find every TextBlock in the main grid and repeat the following statements for each of them
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
-                // Pick a random number between 0 and the number of emoji left in the list
-                int index = random.Next(emoji.Count);
-                // Use the random number called "index" to get a random emoji from the list
-                string nextEmoji = emoji[index];
-                // Update the TextBlock with the random emoji from the list                
-                textBlock.Text = nextEmoji;
-                //Remove the random emoji from the list
-                emoji.RemoveAt(index);
+                if (textBlock.Name != "timeTextBlock")
+                {
+                    textBlock.Visibility = Visibility.Visible;
+                    // Pick a random number between 0 and the number of emoji left in the list
+                    int index = random.Next(emoji.Count);
+                    // Use the random number called "index" to get a random emoji from the list
+                    string nextEmoji = emoji[index];
+                    // Update the TextBlock with the random emoji from the list                
+                    textBlock.Text = nextEmoji;
+                    //Remove the random emoji from the list
+                    emoji.RemoveAt(index);
+                }                
             }
+
+            timer.Start();
+            tenthsOfSecondElapsed = 0;
+            matchesFound = 0;
         }
 
 
@@ -72,6 +103,7 @@ namespace MatchGame
             // Second click if same emoji
             else if (textBlock.Text == lastTextBlockClicked.Text)
             {
+                matchesFound++;
                 // Set the textBlock to be hidden
                 textBlock.Visibility = Visibility.Hidden;
                 // Set findMatching back to false-true match
@@ -84,6 +116,15 @@ namespace MatchGame
                 lastTextBlockClicked.Visibility = Visibility.Visible;
                 // Set findMatching back to false-false match
                 findMatching = false;
+            }
+        }
+
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Resets game if all 8 matched pairs have been found
+            if (matchesFound == 8)
+            {
+                SetUpGame();
             }
         }
     }
