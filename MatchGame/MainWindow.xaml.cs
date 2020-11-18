0ceff2efdd03b0b6d07db723738834e6788cbe49
Phil Paolo <philipcpaolo@gmail.com>
2020-11-18 05:44:38 -0500

    Comment code

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -19,6 +19,7 @@ namespace MatchGame
 
         private void SetUpGame()
         {
+            // Create a list of eight pairs of emojis
             List<string> animalEmoji = new List<string>()
             {
 
@@ -32,13 +33,22 @@ namespace MatchGame
                 "🎂", "🎂",
             };
 
+            // Create a new random number generator
             Random random = new Random();
 
+            // Find every TextBlock in the main grid and repeat the following statements for each of them
             foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
+                // Pick a random number between 0 and the number of emoji left in the list
                 int index = random.Next(animalEmoji.Count);
+
+                // Use the random number called "index" to get a random emoji from the list
                 string nextEmoji = animalEmoji[index];
+
+                // Update the TextBlock with the random emoji from the list 
                 textBlock.Text = nextEmoji;
+
+                //Remove the random emoji from the list
                 animalEmoji.RemoveAt(index);
             }
         }
