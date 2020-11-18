8a18d2deac59a8fa61388b59b40df63ef7a64753
Phil Paolo <philipcpaolo@gmail.com>
2020-11-18 07:40:42 -0500

    Add TextBlock_MouseDown to designer and code

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace MatchGame
 {
@@ -20,7 +21,7 @@ namespace MatchGame
         private void SetUpGame()
         {
             // Create a list of eight pairs of emojis
-            List<string> animalEmoji = new List<string>()
+            List<string> emoji = new List<string>()
             {
 
                 "👀","👀",
@@ -37,19 +38,52 @@ namespace MatchGame
             Random random = new Random();
 
             // Find every TextBlock in the main grid and repeat the following statements for each of them
-            foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
                 // Pick a random number between 0 and the number of emoji left in the list
-                int index = random.Next(animalEmoji.Count);
-
+                int index = random.Next(emoji.Count);
                 // Use the random number called "index" to get a random emoji from the list
-                string nextEmoji = animalEmoji[index];
-
-                // Update the TextBlock with the random emoji from the list 
+                string nextEmoji = emoji[index];
+                // Update the TextBlock with the random emoji from the list                
                 textBlock.Text = nextEmoji;
-
                 //Remove the random emoji from the list
-                animalEmoji.RemoveAt(index);
+                emoji.RemoveAt(index);
+            }
+        }
+
+
+        TextBlock lastTextBlockClicked;
+        bool findMatching = false;
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+
+            // Initial click
+            if (findMatching == false)
+            {
+                // Initial emoji selected make hidden
+                textBlock.Visibility = Visibility.Hidden;
+                // Set the state of lastTextBlockClicked
+                lastTextBlockClicked = textBlock;
+                // Set findMatching to true
+                findMatching = true;
+            }
+            // Second click if same emoji
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                // Set the textBlock to be hidden
+                textBlock.Visibility = Visibility.Hidden;
+                // Set findMatching back to false-true match
+                findMatching = false;
+            }
+            // Second click does not match emoji
+            else
+            {
+                // Make the first emoji visible
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                // Set findMatching back to false-false match
+                findMatching = false;
             }
         }
     }
